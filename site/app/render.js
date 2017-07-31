21fcf824c5f8ceb7826071b8352698a7b0c119f9
杰枫 <huanggbxjp@sohu.com>
2017-07-31 14:04:32 +0800

    webpack config

---
+++ site/app/render.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const render = (Component) => {
+    ReactDOM.render(
+        <Component/>,
+        document.getElementById('app')
+    )
+}
+
+export default render;
