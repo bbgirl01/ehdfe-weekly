68febe5c9c799c2ae419500dd673df4bda8064fe
Jeff Wong <huanggbxjp@sohu.com>
2017-08-02 23:32:40 +0800

    rename many articles

--- site/getArticleList.js
+++ site/getArticleList.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 glob('weekly/*.md',function(err,files){
-    var arr  = files.map((item)=>item.slice(7))
+
+    var arr  = files.map((item)=>item.slice(7)).reverse();
     fs.writeFile('./site/app/globData.json',JSON.stringify(arr),()=>{
         if(err) throw err;
         console.log(chalk.blue("Yes,we have get the article data,go on!"))
