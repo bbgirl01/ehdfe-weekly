833426cb32dc9e8a56f58e3d5d603525e2c9b50e
杰枫 <huanggbxjp@sohu.com>
2017-08-03 17:12:40 +0800

    删除头部的数字

--- site/app/articles/navsData.js
+++ site/app/articles/navsData.js
@@ -5,11 +5,11 @@ const navsData = [];
 var icons = ['coffee','apple','android-o','chrome','github','disconnect','camera-o','laptop','hdd','code-o'];
 
 globData.forEach(function (item) {
-    var reg = /\((.+)\)\.md$/;
+    var reg = /(\d+)\..+\((.+)\)\.md$/;
     var match = reg.exec(item);
     navsData.push({
-        url: '/article/' + match[1],
-        name: item.slice(0,(item.length-match[1].length-5)),
+        url: '/article/' + match[2],
+        name: item.slice((match[1].length+1),(item.length-match[2].length-5)),
         icon: icons[parseInt((Math.random() * 10), 10)],
         item:item
     })
